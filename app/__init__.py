c21cfeb99aee4039d00d85218ce8ebc5d645fc47
Ubuntu <ubuntu@ip-172-31-94-192.ec2.internal>
2020-04-16 02:38:36 +0000

    heroku

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "cheesecake"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://project1:project1@localhost/project1"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://zwmbduvsujxiir:08501009c31f3fd62bb6b5e58151d811717080a2ae96051eeddc0fb0ba19a8fa@ec2-54-243-243-136.compute-1.amazonaws.com:5432/dco681jp648vl5'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
 app.config['UPLOAD_FOLDER'] = './app/static/images'
